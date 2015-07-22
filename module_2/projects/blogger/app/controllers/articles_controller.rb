e8f8b50bdc0296e38e79aa67976ab4c77700a379
mitchashby16 <mitchashby16@gmail.com>
2015-07-22 11:34:05 -0600

    initial commit

---
+++ module_2/projects/blogger/app/controllers/articles_controller.rb
@@ -0,0 +1,17 @@
+class ArticlesController < ActionController::Base
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  def index
+    @articles = Article.all
+  end
+
+  def show
+
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+end
---
+++ module_2/projects/blogger/db/seeds.rb
@@ -0,0 +1,4 @@
+
+Article.create(title: "How to create Blogger", body: "do all of this stuff")
+Article.create(title: "Turing stuff", body: "This is a bunch of stuff about Turing")
+Article.create(title: "Running out of Titles", body: "I'm not very creative")
